2b791154241ba9bf4cf70b8b193d42e6d23f1d10
Jean Tan <riverdiamond11@gmail.com>
2025-07-29 19:52:14 +0800

    attempt online deploy 13

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,7 @@
+// webpack.config.js
 const createExpoWebpackConfigAsync = require('@expo/webpack-config');
 
 module.exports = async function (env, argv) {
   const config = await createExpoWebpackConfigAsync(env, argv);
-
-  config.resolve.fallback = {
-    ...config.resolve.fallback,
-    fs: false,
-    path: false,
-    os: false,
-  };
-
   return config;
 };
