86f82cca2612c3b965d5722888f174980f730626
Jean Tan <riverdiamond11@gmail.com>
2025-07-29 17:56:28 +0800

    attempt to go online 2

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
-const { withExpoWebpack } = require('@expo/webpack-config');
+const createExpoWebpackConfigAsync = require('@expo/webpack-config');
 
 module.exports = async function (env, argv) {
-  return await withExpoWebpack(env, argv);
+  const config = await createExpoWebpackConfigAsync(env, argv);
+  // You can customize the config here if needed
+  return config;
 };
