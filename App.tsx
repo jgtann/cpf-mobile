35f3064b139bd0d9770126822a63d33c8eed9282
Jean Tan <riverdiamond11@gmail.com>
2025-07-29 22:50:12 +0800

    attempt deploy 14

--- App.tsx
+++ App.tsx
@@ -1,15 +1,26 @@
-// File: App.tsx
+// App.tsx
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import React from 'react';
+import { Platform } from 'react-native';
 import InputScreen from './screens/InputScreen';
 import ResultsScreen from './screens/ResultScreen';
 
-const Stack = createNativeStackNavigator();
+const Stack = Platform.OS === 'web'
+  ? require('@react-navigation/stack').createStackNavigator()
+  : require('@react-navigation/native-stack').createNativeStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
+    <NavigationContainer
+      linking={{
+        prefixes: ['/cpf-mobile'],
+        config: {
+          screens: {
+            Input: '',
+            Results: 'results',
+          },
+        },
+      }}
+    >
       <Stack.Navigator initialRouteName="Input">
         <Stack.Screen name="Input" component={InputScreen} />
         <Stack.Screen name="Results" component={ResultsScreen} />
